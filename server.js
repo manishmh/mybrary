e11c1a587ba389c0e67c00b8514936c9f6d72f47
manishmh <manishmh982@gmail.com>
2023-10-28 14:22:13 +0530

    minor

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
 
-app.set('view engine', 'ejs'),
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
